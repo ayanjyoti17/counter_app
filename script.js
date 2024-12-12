aed82c67a318e340ded9659dfe690e0bd747cf20
ayanjyoti17 <jyotiayan47@gmail.com>
2024-12-12 14:39:23 +0530

    getElementById added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const countValue=document.querySelector("#counter");
-const increment=()=>{
+const countValue=document.getElementById("counter");
+const increment=()=>{ //function using arrow
     //get the value from UI
     let value=parseInt(countValue.innerText);//parseInt converts the string text to int value
     //Update the value
@@ -7,7 +7,7 @@ const increment=()=>{
     //set the value onto UI
     countValue.innerText=value;
 };
-const decrement=()=>{
+const decrement=()=>{ 
     //get the value from UI
     let value=parseInt(countValue.innerText);
     //Update the value
